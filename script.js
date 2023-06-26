79517630b9b79301a2134456f5238d287e62c2b9
matt <matt@pop-os.localdomain>
2023-06-26 00:42:32 -0400

    skeleton almost done

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-function test {
-    console.log('test');
-}
+// Wait for the HTML document to load
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the timer element
+    const timerElement = document.getElementById('timer');
+    
+    // Set the initial game time to 0
+    let gameTime = 0;
+    
+    // Function to update the timer display
+    function updateTimer() {
+      timerElement.innerText = `Game Time: ${gameTime} minutes`;
+    }
+    
+    // Function to handle rune and tormentor spawns
+    function handleSpawns() {
+      // Check for bounty rune spawn every 3 minutes
+      if (gameTime % 3 === 0) {
+        console.log('Bounty Rune Spawn!');
+      }
+      
+      // Check for wisdom rune spawn every 7 minutes
+      if (gameTime % 7 === 0) {
+        console.log('Wisdom Rune Spawn!');
+      }
+      
+      // Check for tormentor spawn at 15 minutes
+      if (gameTime === 15) {
+        console.log('The Tormentor Spawn!');
+      }
+    }
+    
+    // Function to increment the game time every minute
+    function incrementTime() {
+      gameTime++;
+      updateTimer();
+      handleSpawns();
+    }
+    
+    // Call the incrementTime function every minute (60,000 milliseconds)
+    setInterval(incrementTime, 60000);
+  });
+  
